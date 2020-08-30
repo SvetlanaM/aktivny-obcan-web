e90c7724ca5e7a38cd0175c6a2d79994f2426fd3
h.t <habrman.tomas@gmail.com>
2020-08-30 20:56:35 +0200

    Initial commit

---
+++ web/themes/custom/aktivnyobcan/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.aktivnyobcan = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
